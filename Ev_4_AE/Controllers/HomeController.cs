cf098f04fd98acc5d516a21e9b7ca364684b4d78
LuxCaro19 <luiscaroretamal2000@gmail.com>
2021-08-02 15:29:26 -0400

    Layouts interna y externa

---
+++ Ev_4_AE/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ev_4_AE.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ActionResult Interna()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Externa()
+        {
+            return View();
+        }
+    }
+}
